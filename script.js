7c638957aa0ae0ef949f6b66f3ab3a75c444aead
AyushPatel-07 <ayushdhakad152@gmail.com>
2025-02-09 15:19:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const codeInput = document.getElementById('code');
 const submitBtn = document.getElementById('submit-btn');
 const resultDiv = document.getElementById('result');
 
-const secretCode = 'Love'; // Replace with your secret code
+const secretCode = 'priyanshi'; // Replace with your secret code
 const nextPageUrl = 'main.html'; // Replace with the URL of the new page
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const userInput = codeInput.value.trim();
 	if (userInput === secretCode) {
-		resultDiv.innerHTML = 'Love';
+		resultDiv.innerHTML = 'priyanshi';
 		window.location.href = nextPageUrl; // Redirect to new page
 	} else {
 		resultDiv.innerHTML = 'Naa Tum meri betu nhi hoo kon hooo tum kaha hai meri betu';
